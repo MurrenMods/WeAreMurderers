a37bc038d1b3bcecfc711993d51253df054a0e13
MurrenMods <merwou@hotmail.com>
2025-06-29 22:52:14 +0200

    Fixed all cpu locations

--- WeAreMurderers/WeAreMurderers/Object/ObjectRegistry.cs
+++ WeAreMurderers/WeAreMurderers/Object/ObjectRegistry.cs
@@ -39,20 +39,21 @@ namespace MurrenMods.WeAreMurderers.Object
             graveprefab.SetGameObject(graveobj);
             graveprefab.Register();
             
-            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo("QX-VR_Grave", new Vector3(347.2f, 155.6f, 908.5f), new Vector3(0, -10f, -20f)));
+            CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo("QX-VR_Grave", new Vector3(348f, 155.4f, 906.4f), new Vector3(-118, -22, -90), new Vector3(45, 45, 45)));
 
-            //TODO: WHERE ARE THEY GOING
+            var align = new Vector3(90, 0, 0);
+            var scale = new Vector3(30, 30, 30);
             var spawns = new List<SpawnInfo>()
             {
-                new SpawnInfo(entries[0].Path, new Vector3(451,-94f, 1176.6f)),
-                new SpawnInfo(entries[1].Path, new Vector3(-882, -309.8f, -811.3f)),
-                new SpawnInfo(entries[2].Path, new Vector3(-1130.3f, -687, -688.4f)),
-                new SpawnInfo(entries[3].Path, new Vector3(-255.7f, -813.5f, 294.7f)),
-                new SpawnInfo(entries[4].Path, new Vector3(-641.4f, -562, 1480.2f)),
-                new SpawnInfo(entries[5].Path, new Vector3(-1220.3f, -399.5f, 1059.3f)),
-                new SpawnInfo(entries[6].Path, new Vector3(-28.3f, -1220.2f, 113)),
-                new SpawnInfo(entries[7].Path, new Vector3(324.8f, -1448.8f, -454.4f)),
-                new SpawnInfo(entries[8].Path, new Vector3(347.2f, 156, 908.5f)),
+                new SpawnInfo(entries[0].Path, new Vector3(449,-93.78f, 1176.6f), align, scale),//DONE
+                new SpawnInfo(entries[1].Path, new Vector3(-881.3f, -309.95f, -811.3f), align, scale),//DONE
+                new SpawnInfo(entries[2].Path, new Vector3(-1130.3f, -687.4f, -688.4f), align, scale),//DONE
+                new SpawnInfo(entries[3].Path, new Vector3(-255.7f, -813.5f, 294.7f), align, scale),//DONE
+                new SpawnInfo(entries[4].Path, new Vector3(-641.4f, -562.5f, 1480.2f), align, scale),//DONE
+                new SpawnInfo(entries[5].Path, new Vector3(-1220.3f, -399.9f, 1059.3f), align, scale),//DONE
+                new SpawnInfo(entries[6].Path, new Vector3(-28.3f, -1220.15f, 113), align, scale),//DONE
+                new SpawnInfo(entries[7].Path, new Vector3(324.8f, -1448.7f, -454.4f), align, scale),//DONE
+                new SpawnInfo(entries[8].Path, new Vector3(347.8f, 155.75f, 906.8f), new Vector3(-118, -22, -90), scale),//DONE
             };
             
             CoordinatedSpawnsHandler.RegisterCoordinatedSpawns(spawns);
